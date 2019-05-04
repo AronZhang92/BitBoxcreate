b51552c359ce19229efa1060f27bfdfc3c5d080e
Aron <wuxingqishi92@gmail.com>
2019-05-04 11:36:44 +1000

    totally successful with some bugs really final version 6

--- src/main/java/unimelb/bitbox/AnotherConnection.java
+++ src/main/java/unimelb/bitbox/AnotherConnection.java
@@ -1,18 +1,29 @@
 package unimelb.bitbox;
 
+import unimelb.bitbox.util.Document;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class AnotherConnection {
 
-    public static void AnotherConnection(String address,int port){
+    public static void AnotherConnection(ArrayList<Document> address){
         try {
-            Socket socket = new Socket(address,port);
-            peerworker w = new peerworker(socket);
-            Thread t = new Thread(w);
-            t.start();
-
+            for (Document dou : address) {
+                int port = dou.getInteger("port");
+                String IPaddress = dou.getString("IPadress");
+                if (!Connectionlist.contain(dou.getString("IPadress"))) {
+                    Socket socket = new Socket(IPaddress, port);
+                    peerworker w = new peerworker(socket);
+                    Thread t = new Thread(w);
+                    t.start();
+                    break;
+                } else {
+                    System.out.println("The system return null post, try another one");
+                }
+            }
         } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
